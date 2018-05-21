ce8f817c33c94288a0e83b05927d1f08d5e46193
Denny Flämig <denny-flaemig@t-online.de>
2018-05-21 15:49:59 +0200

    improved tests

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -10,9 +10,35 @@ public class CustomerTest {
 	public void testCustomerName() {
 		assertEquals(TestConstants.CUSTOMER_NAME, TestConstants.TEST_COSTUMER.getName());
 	}
-	
+
 	@Test
-	public void testCustomerStatement() {
-		assertNotNull(TestConstants.TEST_COSTUMER.statement());
+	public void testCustomerStatementNoRental() {
+		assertEquals("Rental Record for " + TestConstants.CUSTOMER_NAME + "\n" + "\t" + "Title" + "\t" + "\t" + "Days"
+				+ "\t" + "Amount" + "\n" + "Amount owed is " + 0.0 + "\n" + "You earned " + 0
+				+ " frequent renter points", TestConstants.TEST_COSTUMER.statement());
+	}
+
+	@Test
+	public void testCustomerStatementOneRental() {
+		TestConstants.TEST_COSTUMER_ONE_RENTAL.addRental(TestConstants.TEST_RENTAL_1);
+		System.out.println(TestConstants.TEST_COSTUMER_ONE_RENTAL.statement());
+		assertEquals(
+				"Rental Record for " + TestConstants.CUSTOMER_NAME + "\n" + "\t" + "Title" + "\t" + "\t" + "Days" + "\t"
+						+ "Amount" + "\n" + "\t" + "Terminator 1" + "\t" + "\t" + 666 + "\t" + 0.0 + "\n"
+						+ "Amount owed is " + 0.0 + "\n" + "You earned " + 1 + " frequent renter points",
+				TestConstants.TEST_COSTUMER_ONE_RENTAL.statement());
+	}
+
+	@Test
+	public void testCustomerStatementTwoRentals() {
+		TestConstants.TEST_COSTUMER_TWO_RENTALS.addRental(TestConstants.TEST_RENTAL_1);
+		TestConstants.TEST_COSTUMER_TWO_RENTALS.addRental(TestConstants.TEST_RENTAL_2);
+		System.out.println(TestConstants.TEST_COSTUMER_TWO_RENTALS.statement());
+		assertEquals(
+				"Rental Record for " + TestConstants.CUSTOMER_NAME + "\n" + "\t" + "Title" + "\t" + "\t" + "Days" + "\t"
+						+ "Amount" + "\n" + "\t" + "Terminator 1" + "\t" + "\t" + 666 + "\t" + 0.0 + "\n" + "\t"
+						+ "Terminator 2" + "\t" + "\t" + 42 + "\t" + 0.0 + "\n" + "Amount owed is " + 0.0 + "\n"
+						+ "You earned " + 2 + " frequent renter points",
+				TestConstants.TEST_COSTUMER_TWO_RENTALS.statement());
 	}
 }
--- src/test/TestConstants.java
+++ src/test/TestConstants.java
@@ -12,4 +12,6 @@ public class TestConstants {
 	public static final Rental TEST_RENTAL_1 = new Rental(TEST_MOVIE_1, 666);
 	public static final Rental TEST_RENTAL_2 = new Rental(TEST_MOVIE_2, 42);
 	public static final Customer TEST_COSTUMER = new Customer(CUSTOMER_NAME);
+	public static final Customer TEST_COSTUMER_ONE_RENTAL = new Customer(CUSTOMER_NAME);
+	public static final Customer TEST_COSTUMER_TWO_RENTALS = new Customer(CUSTOMER_NAME);
 }
