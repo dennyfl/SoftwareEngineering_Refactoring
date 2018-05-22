ef0041caf1d1cdd02b59d6ca166e891acbf5a06b
Denny Flämig <denny-flaemig@t-online.de>
2018-05-22 22:40:00 +0200

    move method
    moved getCharge() from movie to price

--- src/denny/Movie.java
+++ src/denny/Movie.java
@@ -5,7 +5,7 @@ public class Movie {
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
 	private String title;
-	//	private int priceCode;
+	// private int priceCode;
 	private Price price;
 
 	public Movie(String name, int priceCode) {
@@ -38,23 +38,7 @@ public class Movie {
 	}
 
 	double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
+		return price.getCharge(daysRented);
 	}
 
 	int getFrequentRenterPoints(int daysRented) {
